45bd302c903ef4ba4b92ff807a5dfd2ab5495a4c
Pedro Barbosa <49070051+154Nacho@users.noreply.github.com>
2021-01-04 00:07:35 +0000

    Add files via upload

---
+++ FASE3/src/dss_project_fase3/utils/Enums/ZonaArmazem.java
@@ -0,0 +1,8 @@
+package dss_project_fase3.utils.Enums;
+
+/**
+ * Classe com Um ENUM relativo ao tipo de zona (da Localização) onde um objeto se pode encontrar
+ */
+public enum ZonaArmazem {
+    ZONA_RECECAO, ZONA_ENTREGA, ZONA_ARMAZENAMENTO, ROBOT;
+}
