4411697d68f27bd23862c549d9016e033c187bf7
Pedro Barbosa <49070051+154Nacho@users.noreply.github.com>
2021-01-04 00:09:57 +0000

    Add files via upload

---
+++ FASE3/src/dss_project_fase3/utils/Enums/RobotRequest.java
@@ -0,0 +1,8 @@
+package dss_project_fase3.utils.Enums;
+
+/**
+ * Classe com Um ENUM relativo ao tipo de request do robot
+ */
+public enum RobotRequest {
+    RECOLHA, ENTREGA;
+}
---
+++ FASE3/src/dss_project_fase3/utils/Enums/TransportOrderError.java
@@ -0,0 +1,5 @@
+package dss_project_fase3.utils.Enums;
+
+public enum TransportOrderError {
+    ARMAZEM_CHEIO, ROBOTS_INDISPONIVEIS;
+}
