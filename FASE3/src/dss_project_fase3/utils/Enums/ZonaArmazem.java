8062fb162b58ef350167372c9512c779db24194e
Pedro Barbosa <49070051+154Nacho@users.noreply.github.com>
2021-01-04 00:08:02 +0000

    Delete ZonaArmazem.java

--- FASE3/src/dss_project_fase3/utils/Enums/ZonaArmazem.java
+++
@@ -1,8 +0,0 @@
-package dss_project_fase3.utils.Enums;
-
-/**
- * Classe com Um ENUM relativo ao tipo de zona (da Localização) onde um objeto se pode encontrar
- */
-public enum ZonaArmazem {
-    ZONA_RECECAO, ZONA_ENTREGA, ZONA_ARMAZENAMENTO, ROBOT;
-}
